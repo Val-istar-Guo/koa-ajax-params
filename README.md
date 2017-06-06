f2cb681f7a167eed78a9b12ed89b6da25bf86c10
Val.istar.Guo <Val.istar.Guo@gmail.com>
2017-06-06 14:14:06 +0800

    base

---
+++ README.md
@@ -0,0 +1,25 @@
+# KOA-AJAX-PARAMS
+
+The Koa Middleware used to auto parse get & post request params.
+
+## Install
+
+```bash
+npm i koa-ajax-params
+```
+
+```bash
+yarn add kao-ajax-params
+```
+
+## Usage
+
+```javascript
+import Koa from 'koa';
+import router from 'koa-router';
+import ajaxParams from 'koa-ajax-params';
+
+const app = new Koa();
+const route = new router();
+```
+
---
+++ src/index.js
@@ -0,0 +1,68 @@
+// import querystring from 'querystring';
+
+
+function getBody(req) {
+  return new Promise((resolve, reject) => {
+    let data = '';
+    req
+      .on('data', (chunk) => {
+        data += chunk;
+      })
+      .on('end', () => {
+        resolve(data);
+      })
+      .on('error', reject);
+  });
+}
+
+const Handles = {
+  'application/json': function (data) {
+    return JSON.parse(data);
+  },
+};
+
+async function getRequestHandle(ctx) {
+  return ctx.query;
+}
+
+async function postRequestHandle(ctx) {
+  if (ctx.method !== 'POST') return null;
+
+  let params = null;
+  const body = await getBody(ctx.req);
+  const type = ctx.get('Content-type').toLowerCase();
+
+  if (type in Handles) {
+    const handle = Handles[type];
+
+    params = handle(body);
+  }
+
+  return params;
+}
+
+
+function defaultFilter(params, method) {
+  return params;
+}
+
+/**
+ * @desc auto parse ajax params into ctx.params
+ *
+ * @param filter {Function} Handle params after parse
+ */
+export default function ({ filter = defaultFilter } = {}) {
+  return async (ctx, next) => {
+    const app = ctx.app;
+
+    let get = await getRequestHandle(ctx);
+    let post = await postRequestHandle(ctx);
+
+    // Apply filter
+    get = filter(get);
+    post = filter(post);
+
+    app.context.params = { get, post };
+    await next();
+  };
+}
